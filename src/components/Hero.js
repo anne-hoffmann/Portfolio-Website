137ac0b7b39498465da38501dcb5a37042921096
Anestasia Hoffmann <anestasiahoffmann@gmail.com>
2024-01-16 17:18:20 +0200

    Bug Fixed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,10 +14,10 @@ const Hero = () => {
                     </a>
                 </div>
 
-                <div>
+                <div className="md:w-[500px] md:h-[500px] w-[300px] h-[300px] rounded-full overflow-hidden relative">
                     <img src={HeroImg}
                     alt="hero-img"
-                    className="md:w-[500px] md:h-[500px] w-[300px] h-[300px] rounded full overflow-hidden relative" />
+                    className="w-full h-full object-cover" />
                 </div>
 
             </div>
