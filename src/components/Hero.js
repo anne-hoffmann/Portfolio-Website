5cb57dc8b982475c6c2b04458a6674d12aac08bb
Anestasia Hoffmann <anestasiahoffmann@gmail.com>
2024-01-16 17:35:03 +0200

    button fixed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
                         Hi! <br /> I am Anne, <br /> Frontend Developer.
                     </h1>
                     <a href='/#projects'
-                    className="w-32 h-12 bg-[#faf9fa] border-2 py-2 px-4 border-[#05040a] rounded-full hover:bg-transparent"
+                    className="w-40 h-12 bg-[#faf9fa] border-2 py-2 px-4 border-[#05040a] rounded-full hover:bg-transparent"
                     >See Projects
                     </a>
                 </div>
